2186c3b066cd6706428e361d005e62484af38a26
playBit1 <binulben5@gmail.com>
2024-04-02 21:25:31 +1100

    V1 commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,77 @@
 $(document).ready(function () {
+  getCards();
     $('.modal').modal()
   })
+
+  // Function to create a card
+function createCard(cardData) {
+  return `
+  <div class="col s12 m4">
+      <div class="card medium">
+          <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="${cardData.path}" />
+          </div>
+          <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4" style="padding-left: 20px">${cardData.title}
+              <i class="material-icons right">more_vert</i>
+              </span>
+          </div>
+          <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">${cardData.title}
+              <i class="material-icons right">close</i>
+              </span>
+              <p class="grey-text">${cardData.description}</p>
+          </div>
+      </div>
+  </div>
+  `;
+}
+
+const getCards = () => {
+  $.get('/api/cars', (response) => {
+    if(response.statusCode==200){
+      const cardContainer = document.getElementById('CarCards');
+      let catCardList = response.data
+
+      console.log(response.data)
+      cardContainer.innerHTML = '';
+
+      catCardList.forEach(cardData => {
+        const card = createCard(cardData);
+        cardContainer.innerHTML += card;
+      });
+      
+    }
+  })
+}
+
 
   function submitInfo(e) {
     e.preventDefault()
 
     let formData = {
-      first_name : document.getElementById('first_name').value,
-      last_name : document.getElementById('last_name').value,
-      email : document.getElementById('email').value,
-      password : document.getElementById('password').value
+      title : document.getElementById('title').value,
+      color : document.getElementById('color').value,
+      path : document.getElementById('path').value,
+      description : document.getElementById('description').value
     };
   
     console.log("Form Data Submitted: ", formData)
 
-    document.getElementById('first_name').value = '';
-    document.getElementById('last_name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('password').value = '';
+    $.post("/api/cats", formData, function(data, status){
+      status === "success" ? (alert("Card Information Saved!"), getCards()): alert("Error, please contact car support!")
+    })
+
+    document.getElementById('title').value = '';
+    document.getElementById('color').value = '';
+    document.getElementById('path').value = '';
+    document.getElementById('description').value = '';
 
     var instance = M.Modal.getInstance(document.getElementById('modal1'));
     instance.close();
 
+    getCards()
+    getCards()
+    getCards()
+    getCards()
   }
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ben:42dg25I66ZgyuBVc@cluster0.mpmybdn.mongodb.net/?retryWrites=true&w=majority";
 var app = express()
+
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json())
@@ -7,7 +10,63 @@ app.use(express.urlencoded({extended: false}))
 
 var port = process.env.port || 3000;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict:true,
+        deprecationErrors:true
+    }
+});
+
+let catCardList = [];
+
+async function run() {
+    try {
+    await client.connect();
+    const database = client.db('SIT725_proj4');
+    const cats = database.collection('Cats');
+
+    catCardList = await cats.find({}, { projection: { _id: 0 } }).toArray();
+    
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+app.get('/', (req, res) => {res.render('index.html')}); 
+
+app.get('/api/cars', (req,res) => {
+    run()
+    res.json({
+        statusCode: 200,
+        data: catCardList,
+        message: "Success"
+    })
+})
+
+app.post('/api/cats', async (req, res) => {
+    const newCat = req.body;
+    const database = client.db('SIT725_proj4');
+    const cats = database.collection('Cats');
+
+    try {
+        await cats.insertOne(newCat);
+        res.json({
+            statusCode: 200,
+            message: "Success"
+        })
+    } catch (err) {
+        console.error(err);
+        res.json({
+            statusCode: 500,
+            message: "Error inserting record"
+        })
+    }
+
+})
 
 app.listen(port,()=>{
-console.log("App listening to: http://localhost:"+port)
+    run()
+    console.log("App listening to: http://localhost:"+port)
 })
